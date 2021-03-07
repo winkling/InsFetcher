4f42f5320d5f7ef325283bd0b09327d40bd77b45
Kun Wang <mailwangkun@gmail.com>
2021-03-07 00:00:30 -0800

    Add FileContact

---
+++ FileConcat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace FileConcat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var basePath = @"C:\Temp\20210306";
+            var dstPath = Path.Combine(basePath, "output.mp4");
+
+            using (BinaryWriter sw = new BinaryWriter(File.OpenWrite(dstPath)))
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    var path = string.Format("{0}\\{1}.mp4", basePath, i);
+                    var bytes = File.ReadAllBytes(path);
+                    sw.Write(bytes);
+                }
+            }
+        }
+    }
+}
